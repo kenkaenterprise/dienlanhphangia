5927b8b026f8b780ba39114b9c961a75d90405c3
Hoang Phan <hoangpv@smartosc.com>
2022-11-22 15:59:36 -0800

    PHANGIA-44 [dienlanhphangia.com] Add more phone numbers

--- web/app/themes/wp-bootstrap-4/template-parts/header/header-main.php
+++ web/app/themes/wp-bootstrap-4/template-parts/header/header-main.php
@@ -5,7 +5,7 @@
     <section class="header__main">
         <div class="container">
             <div class="row">
-                <div class="col-xl-3 d-none d-xl-block h-100 align-items-center">
+                <div class="col-xl-1 d-none d-xl-block h-100 align-items-center">
                     <div class="logo_area">
                         <a href="<?php echo home_url(); ?>" title=""><img src="<?php $logo = get_field( 'logo', 'options' ); echo $logo['url']; ?>" alt=""></a>
                     </div>
@@ -22,8 +22,18 @@
                     <div class="company_phone d-flex justify-content-center">
                         <div class="icon_phone"><img width="70px" src="https://dienlanhphangia.com/wp-content/uploads/giot%20nuoc.png" alt="""></div>
                         <div class="text_phone">
-                            <p>Hotline:</p>
-                            <p class="m-0 chu-noi phones"><?php the_field( 'hotline', 'options' ); ?></p>
+                            <div>
+                                <p>Hotline:</p>
+                                <p class="m-0 chu-noi phones"><?php the_field( 'hotline', 'options' ); ?></p>
+                            </div>
+                            <div>
+                                <p>Kinh doanh, Dự án:</p>
+                                <p class="m-0 chu-noi phones">0902 980 892</p>
+                            </div>
+                            <div>
+                                <p>Bảo trì, sữa chữa:</p>
+                                <p class="m-0 chu-noi phones">0909 980 892</p>
+                            </div>
                         </div>
                     </div>
                 </div>
