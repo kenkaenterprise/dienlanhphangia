7c472d95be1fd619285ae0a7801b91f0a5ae7895
Hoang Phan <hoangpv@smartosc.com>
2022-11-23 09:35:21 -0800

    PHANGIA-44 [dienlanhphangia.com] Add more phone numbers

--- web/app/themes/wp-bootstrap-4/template-parts/header/header-main.php
+++ web/app/themes/wp-bootstrap-4/template-parts/header/header-main.php
@@ -5,7 +5,7 @@
     <section class="header__main">
         <div class="container">
             <div class="row">
-                <div class="col-xl-3 d-none d-xl-block h-100 align-items-center">
+                <div class="col-xl-2 d-none d-xl-block h-100 align-items-center">
                     <div class="logo_area">
                         <a href="<?php echo home_url(); ?>" title=""><img src="<?php $logo = get_field( 'logo', 'options' ); echo $logo['url']; ?>" alt=""></a>
                     </div>
@@ -22,9 +22,20 @@
                     <div class="company_phone d-flex justify-content-center">
                         <div class="icon_phone"><img width="70px" src="https://dienlanhphangia.com/wp-content/uploads/giot%20nuoc.png" alt="""></div>
                         <div class="text_phone">
-                            <p>Hotline:</p>
-                            <p class="m-0 chu-noi phones"><?php the_field( 'hotline', 'options' ); ?></p>
+                            <div>
+                                <p class="title">Hotline:</p>
+                                <p class="number chu-noi"><?php the_field( 'hotline', 'options' ); ?></p>
+                            </div>
+                            <div>
+                                <p class="title">Kinh doanh:</p>
+                                <p class="number chu-noi"><?php the_field( 'hotline_kinhdoanh', 'options' ); ?></p>
+                            </div>
+                            <div>
+                                <p class="title">Sửa chữa:</p>
+                                <p class="number chu-noi"><?php the_field( 'hotline_baotri', 'options' ); ?></p>
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </div>
