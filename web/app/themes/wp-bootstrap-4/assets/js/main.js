3d0119401962f32d100e706e314f7b701ea76edf
Dieter R <dieter.roark@getkenka.com>
2024-10-04 05:31:48 -0400

    Node-584-add-block-XYZ-into-homepage Fix feedback

--- web/app/themes/wp-bootstrap-4/assets/js/main.js
+++ web/app/themes/wp-bootstrap-4/assets/js/main.js
@@ -290,9 +290,7 @@ var swiper7 = new Swiper('.swiper7', {
   new Swiper('.certificates-carousel', {
     slidesPerView: 3,
     spaceBetween: 12,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    navigation: {
       nextEl: '.swiper-button-next_8',
       prevEl: '.swiper-button-prev_8'
     },
